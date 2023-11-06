1d05da62ae774c131fbfb110c71f58ede5ac2f55
Ernst Jendritzki <ernstjendritzki@web.de>
2023-11-06 22:58:12 +0100

    Declaration

--- system/modules/isotope/library/Isotope/Weight.php
+++ system/modules/isotope/library/Isotope/Weight.php
@@ -4,16 +4,17 @@ namespace Isotope;
 
 use Contao\StringUtil;
 use Contao\System;
-use Haste\Units\Dimension\Unit;
 use Isotope\Interfaces\IsotopeWeighable;
 use UnitConverter\UnitConverter;
 
 class Weight implements IsotopeWeighable
 {
+    private static UnitConverter $unitConverter;
+
     public function __construct(
         private float $fltValue,
-        private string $strUnit)
-    {
+        private string $strUnit
+    ) {
     }
 
     public function getWeightValue()
@@ -34,15 +35,17 @@ class Weight implements IsotopeWeighable
     public static function createFromTimePeriod($arrData)
     {
         $container = System::getContainer();
-        $unitConverter = $container->get('isotope.unit_converter');
+        self::$unitConverter = $container->get('isotope.unit_converter');
 
         $arrData = StringUtil::deserialize($arrData);
 
-        if (empty($arrData)
+        if (
+            empty($arrData)
             || !is_array($arrData)
             || $arrData['value'] === ''
             || $arrData['unit'] === ''
-            || !in_array($arrData['unit'], $unitConverter->getRegistry()->listUnits('Mass'))) {
+            || !in_array($arrData['unit'], self::$unitConverter->getRegistry()->listUnits('Mass'))
+        ) {
             return null;
         }
 
