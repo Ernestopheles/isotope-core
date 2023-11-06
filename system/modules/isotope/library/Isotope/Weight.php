a3ce73da9072cd9a625bf390ec24c0eb25bc8fec
Ernst Jendritzki <ernstjendritzki@web.de>
2023-11-06 22:50:04 +0100

    legacy mode ...

--- system/modules/isotope/library/Isotope/Weight.php
+++ system/modules/isotope/library/Isotope/Weight.php
@@ -13,9 +13,7 @@ class Weight implements IsotopeWeighable
     public function __construct(
         private float $fltValue,
         private string $strUnit,
-        private UnitConverter $unitConverter)
     {
-        $this->unitConverter = $unitConverter ?? System::getContainer()->get('isotope.unit_converter');
     }
 
     public function getWeightValue()
@@ -35,18 +33,19 @@ class Weight implements IsotopeWeighable
      */
     public static function createFromTimePeriod($arrData)
     {
-        $arrData = StringUtil::deserialize($arrData);
+        $container = System::getContainer();
+        $unitConverter = $container->get('isotope.unit_converter');
 
-        $weight =  new Weight($arrData['value'], $arrData['unit']);
+        $arrData = StringUtil::deserialize($arrData);
 
         if (empty($arrData)
             || !is_array($arrData)
             || $arrData['value'] === ''
             || $arrData['unit'] === ''
-            || !in_array($arrData['unit'], self::$unitConverter->getRegistry()->listUnits('Mass'))) {
+            || !in_array($arrData['unit'], $unitConverter->getRegistry()->listUnits('Mass'))) {
             return null;
         }
 
-        return $weight;
+        return new static($arrData['value'], $arrData['unit']);
     }
 }
